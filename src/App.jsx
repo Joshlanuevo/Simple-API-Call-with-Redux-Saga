718517dc6cdcf3678701b1393fcddceef08becd5
Joshlanuevo <lanuevo68@gmail.com>
2023-12-10 22:04:27 +0800

    reducer added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { getUsersFetch } from './actions';
+
 function App() {
+  const dispatch = useDispatch();
+  const users = useSelector(state => state.myFirstReducer.users);
   return (
     <div className="App">
       
