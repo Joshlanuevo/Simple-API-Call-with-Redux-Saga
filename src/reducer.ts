c225cf84ba17989afafe3c21d19dc8575be5a9d9
Joshlanuevo <lanuevo68@gmail.com>
2023-12-10 21:59:36 +0800

    saga implemented.

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,5 +1,13 @@
-const myFirstReducer = (state = {}, action) => {
+import { GET_USERS_SUCCESS, GET_USERS_FETCH } from "./actions";
+
+const myFirstReducer = (state = { users: [] }, action) => {
     switch (action.type) {
+        case GET_USERS_SUCCESS: 
+            return {
+                ...state,
+                users: action.users
+            }
+        // case GET_USERS_FETCH:
         default: 
             return state;
     }
